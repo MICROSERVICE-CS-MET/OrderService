36e2797f978014742cef9047d73136e05c15f1bc
Mustafa Enes Tepe <mustafatpe23@gmail.com>
2023-08-17 09:55:49 +0300

    add productName

--- src/main/kotlin/com/example/orderservice/domain/event/OrderItem.kt
+++ src/main/kotlin/com/example/orderservice/domain/event/OrderItem.kt
@@ -2,6 +2,7 @@ package com.example.orderservice.domain.event
 
 data class OrderItem(
     var productId: String?,
+    var productName:String?,
     var productCount: Int?,
     var unitPrice: Double?,
     var totalPrice: Double?
--- src/main/kotlin/com/example/orderservice/domain/model/OrderItem.kt
+++ src/main/kotlin/com/example/orderservice/domain/model/OrderItem.kt
@@ -2,6 +2,7 @@ package com.example.orderservice.domain.model
 
 data class OrderItem(
     var productId: String?,
+    var productName: String?,
     var productCount: Int?,
     var unitPrice: Double?,
     var totalPrice: Double?
